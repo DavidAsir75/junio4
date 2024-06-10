a81fcf19a014b5d0dfeb94e3a953ae05df890ae3
David <David@DESKTOP-IH8QDQ3>
2024-06-10 22:13:02 +0200

    clase jefe

---
+++ src/junio4/Jefe.java
@@ -0,0 +1,50 @@
+package junio4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jefe extends Empleado {
+    private static final int MIN_PROYECTOS = 1;
+    public static final int MAX_PROYECTOS = 3;
+    private double salarioDiario;
+    private List<Proyecto> proyectos;
+
+    public Jefe() {
+        super();
+        this.proyectos = new ArrayList<>();
+    }
+
+    public Jefe(String nombre, double salarioDiario) {
+        super(nombre);
+        this.salarioDiario = salarioDiario;
+        this.proyectos = new ArrayList<>();
+    }
+
+    @Override
+    public void asignarProyecto(Proyecto proyecto) {
+        if (proyectos.size() >= MIN_PROYECTOS && proyectos.size() < MAX_PROYECTOS) {
+            proyectos.add(proyecto);
+            proyecto.agregarEmpleado(this);
+        } else if (proyectos.size() < MIN_PROYECTOS) {
+            proyectos.add(proyecto);
+            proyecto.agregarEmpleado(this);
+        } else {
+            throw new IllegalArgumentException("Error: Este jefe ya tiene 3 proyectos asignados.");
+        }
+    }
+
+    @Override
+    public double getSalarioDiario() {
+        return salarioDiario;
+    }
+
+    @Override
+    public List<Proyecto> getProyectos() {
+        return proyectos;
+    }
+    
+    @Override
+    public String toString() {
+        return "Jefe: " + getNombre() + ", Salario Diario: " + getSalarioDiario() + ", Proyectos Asignados: " + proyectos.size();
+    }
+}
