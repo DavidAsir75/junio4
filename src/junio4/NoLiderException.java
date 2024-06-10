e44a872346045776e75f9d7d4097b444a4bb0985
David <David@DESKTOP-IH8QDQ3>
2024-06-10 22:47:05 +0200

    noliderexception2

--- src/junio4/NoLiderException.java
+++ src/junio4/NoLiderException.java
@@ -1,5 +1,7 @@
 package junio4;
 
-public class NoLiderException {
-
+public class NoLiderException extends Exception {
+    public NoLiderException() {
+        super("Error: Este programador ya tiene el máximo de proyectos asignados o no es líder.");
+    }
 }
