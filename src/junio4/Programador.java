1d2c1078692770d029ffa45c15cd867a8d313b48
David <David@DESKTOP-IH8QDQ3>
2024-06-10 22:34:53 +0200

    Clase programador con interfaz eslider

--- src/junio4/Programador.java
+++ src/junio4/Programador.java
@@ -3,12 +3,7 @@ package junio4;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Programador extends Empleado {
-    private static final int MIN_PROYECTOS_NORMAL = 5;
-    private static final int MAX_PROYECTOS_NORMAL = 25;
-    private static final int MIN_PROYECTOS_LIDER = 1;
-    private static final int MAX_PROYECTOS_LIDER = 10;
-    
+public class Programador extends Empleado implements EsLider {
     private double salarioDiario;
     private boolean esLider;
     private List<Proyecto> proyectos;
@@ -18,8 +13,8 @@ public class Programador extends Empleado {
         this.proyectos = new ArrayList<>();
     }
 
-    public Programador(String nombre, double salarioDiario, boolean esLider) {
-        super(nombre);
+    public Programador(int id, String nombre, double salarioDiario, boolean esLider) {
+        super(id, nombre);
         this.salarioDiario = salarioDiario;
         this.esLider = esLider;
         this.proyectos = new ArrayList<>();
@@ -27,14 +22,11 @@ public class Programador extends Empleado {
 
     @Override
     public void asignarProyecto(Proyecto proyecto) {
-        if (esLider && proyectos.size() < MAX_PROYECTOS_LIDER) {
-            proyectos.add(proyecto);
-            proyecto.agregarEmpleado(this);
-        } else if (!esLider && proyectos.size() >= MIN_PROYECTOS_NORMAL && proyectos.size() < MAX_PROYECTOS_NORMAL) {
+        if ((esLider && proyectos.size() < 10) || (!esLider && proyectos.size() == 0)) {
             proyectos.add(proyecto);
             proyecto.agregarEmpleado(this);
         } else {
-            System.out.println("Error: Este programador ya tiene el máximo de proyectos asignados o no cumple con el mínimo requerido.");
+            System.out.println("Error: Este programador ya tiene el máximo de proyectos asignados o no es líder.");
         }
     }
 
@@ -43,7 +35,8 @@ public class Programador extends Empleado {
         return esLider ? salarioDiario * 1.25 : salarioDiario;  // Plus del 25% si es líder
     }
 
-    public boolean isEsLider() {
+    @Override
+    public boolean isLider() {
         return esLider;
     }
 
@@ -52,4 +45,3 @@ public class Programador extends Empleado {
         return proyectos;
     }
 }
-
